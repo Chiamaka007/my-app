38f508e9f77692c1c3e51fbdcf7604cf83e92909
Chiamaka Ugoji <mychiamaka@gmail.com>
2021-09-22 22:11:53 +0100

    Added weather app html

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <Weather />
+      <div className="container">
+        <Weather />
+      </div>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,50 +1,46 @@
-import React, { useState } from "react";
+import React from "react";
 import axios from "axios";
 
-import "./App.css";
+import "./Weather.css";
 
 export default function Weather() {
-  const [city, setCity] = useState(null);
-  const [weather, setWeather] = useState(null);
-
-  function showWeather(response) {
-    console.log(response);
-    let temperature = response.data.main.temp;
-    let wind = response.data.wind.speed;
-    let humidity = response.data.main.humidity;
-    let description = response.data.weather[0].description;
-    let icon = `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`;
-    setWeather(
-      <ul className="weatherSearch">
-        <li>Temperature: {Math.round(temperature)}ºC </li>
-        <li>Description: {description}</li>
-        <li>Humidity: {humidity}%</li>
-        <li>Wind: {wind} km/h</li>
-        <li>
-          <img src={icon} alt={description} />
-        </li>
-      </ul>
-    );
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    let apiKey = "5447915a7bcf0a835af038a7bf56c3a2";
-    let units = "metric";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
-    axios.get(apiUrl).then(showWeather);
-  }
-  function changeCity(event) {
-    setCity(event.target.value);
-  }
-
   return (
     <div className="Weather">
-      <form onSubmit={handleSubmit}>
-        <input type="search" placeholder="Type a city" onChange={changeCity} />
-        <input type="submit" value="Search" />
+      <form>
+        <div class="row">
+          <div class="col-9">
+            <input
+              type="Search"
+              placeholder="Search city"
+              className="form-control"
+            />
+          </div>
+          <div class="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
       </form>
-      <div>{weather}</div>
+      <h1>Lagos</h1>
+      <ul>
+        <li>Last updated at 12:00</li>
+        <li>Clear with periodic clouds</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
+            alt="weather"
+          />
+          26°C
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitaion: 9%</li>
+            <li>Humidity: 90%</li>
+            <li>Wind :3km/h</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
