f7825dc8ead655e35fc0370a982d54acd95b917d
Chiamaka Ugoji <mychiamaka@gmail.com>
2021-10-01 21:54:44 +0100

    added api integration

--- src/Weather.js
+++ src/Weather.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React,{useState} from "react";
 import axios from "axios";
 
 import "./Weather.css";
 
 export default function Weather() {
-  return (
+  const [weather, setWeather] = useState({ ready: false });
+
+   function handleResponse(response) {
+     console.log(response);
+      setWeather({
+        ready: true,
+        coordinates: response.data.coord,
+        temperature: response.data.main.temp,
+        humidity: response.data.main.humidity,
+        date: new Date(response.data.dt * 1000),
+        description: response.data.weather[0].description,
+        icon: response.data.weather[0].icon,
+        wind: response.data.wind.speed,
+        city: response.data.name,
+      });
+   }
+
+  
+  
+  if (weather.ready) {
+    return (
     <div className="Weather">
       <form>
         <div class="row">
@@ -25,10 +45,10 @@ export default function Weather() {
           </div>
         </div>
       </form>
-      <h1>Lagos</h1>
+      <h1>{weather.city}</h1>
       <ul>
         <li>Last updated at 12:00</li>
-        <li>Clear with periodic clouds</li>
+        <li>{weather.description}</li>
       </ul>
       <div className="mt-3 row">
         <div className="col-6">
@@ -39,18 +59,26 @@ export default function Weather() {
               className="float-left"
             />
 
-            <span className="temperature">26</span>
+            <span className="temperature">{Math.round(weather.temperature)}</span>
             <span class="unit">°C</span>
           </div>
         </div>
         <div className="col-6">
           <ul>
-            <li>Precipitation: 9%</li>
-            <li>Humidity: 90%</li>
-            <li>Wind :3km/h</li>
+        
+            <li>Humidity: {weather.humidity}%</li>
+            <li>Wind : {weather.wind}km/h</li>
           </ul>
         </div>
       </div>
     </div>
-  );
+
+  ); 
+} else {
+  const apiKey = "5447915a7bcf0a835af038a7bf56c3a2";
+  let city = "Lagos";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+  axios.get(apiUrl).then(handleResponse);
+  return "loading.."
+  }
 }
