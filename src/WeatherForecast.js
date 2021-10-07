53a2610282da9f6adc555bd35a921cd29032f1db
Chiamaka Ugoji <mychiamaka@gmail.com>
2021-10-07 15:35:08 +0100

    added states

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,27 +1,37 @@
 import axios from "axios";
-import React from "react";
+import React, { useState } from "react";
 import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherForecast(props) {
+  const [loaded, setLoaded] = useState(false);
+  const [forecast, setForecast] = useState(null);
+
   function handleResponse(response) {
+    setForecast(response.data.daily);
+    setLoaded(true);
+
     console.log(response.data);
   }
-  let lat = props.coordinates.lat;
-  let lon = props.coordinates.lat;
-  const apiKey = "5447915a7bcf0a835af038a7bf56c3a2";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
-  axios.get(apiUrl).then(handleResponse);
-  return (
-    <div className="WeatherForecast">
-      <div className="row">
-        <div className="col">
-          <div>Thu</div>
-          <WeatherIcon code="01d" />
-          <br />
-          <span className="temperature-min">20</span>
-          <span className="temperature-max"> 24</span>
+
+  if (loaded) {
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          <div className="col">
+            <div>Thu</div>
+            <WeatherIcon code="01d" />
+            <br />
+            <span className="temperature-min">{forecast[0].temp.min}</span>
+            <span className="temperature-max"> {forecast[0].temp.max}</span>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let lat = props.coordinates.lat;
+    let lon = props.coordinates.lat;
+    const apiKey = "5447915a7bcf0a835af038a7bf56c3a2";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
 }
